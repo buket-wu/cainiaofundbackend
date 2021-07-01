8d774311227716c3e55667b8985762eb5927656c
wujiajin <wujiajin@sensetime.com>
2021-07-01 20:22:07 +0800

    add main and go.sum

--- main.go
+++ main.go
@@ -4,13 +4,10 @@ import (
 	"cainiaofundbackend/crontab"
 	"cainiaofundbackend/logger"
 	"github.com/sirupsen/logrus"
-	"time"
 )
 
 func main() {
 	logger.InitLogrus()
 	crontab.InitCron()
 	logrus.Info("dddds")
-
-	time.Sleep(5 * time.Minute)
 }
